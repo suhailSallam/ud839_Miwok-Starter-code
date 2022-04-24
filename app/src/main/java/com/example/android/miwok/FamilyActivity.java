771212e4afe4f5c492c10d40f95630b4d9afc708
Suhail.sallam <Suhail.sallam@gmail.com>
2022-04-24 22:31:49 +0300

    last version

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -15,16 +15,16 @@ public class FamilyActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
         //Create a list of words
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word ("أب","father",R.drawable.family_father));
-        words.add(new Word( "أم","mother",R.drawable.family_mother));
-        words.add(new Word("إبن","son",R.drawable.family_son));
-        words.add(new Word("إبنة","daughter",R.drawable.family_daughter));
-        words.add(new Word("أخ أكبر","older brother",R.drawable.family_older_brother));
-        words.add(new Word("أخ أصغر","younger brother",R.drawable.family_younger_brother));
-        words.add(new Word("أخت كبرى","older sister",R.drawable.family_older_sister));
-        words.add(new Word("أخت صغرى","younger sister",R.drawable.family_younger_sister));
-        words.add(new Word("جدة","grandmother",R.drawable.family_grandmother));
-        words.add(new Word("جد","grandfather",R.drawable.family_grandfather));
+        words.add(new Word ("أب","father",R.drawable.family_father,R.raw.family_father));
+        words.add(new Word( "أم","mother",R.drawable.family_mother,R.raw.family_mother));
+        words.add(new Word("إبن","son",R.drawable.family_son,R.raw.family_son));
+        words.add(new Word("إبنة","daughter",R.drawable.family_daughter,R.raw.family_daughter));
+        words.add(new Word("أخ أكبر","older brother",R.drawable.family_older_brother,R.raw.family_older_brother));
+        words.add(new Word("أخ أصغر","younger brother",R.drawable.family_younger_brother,R.raw.family_younger_brother));
+        words.add(new Word("أخت كبرى","older sister",R.drawable.family_older_sister,R.raw.family_older_sister));
+        words.add(new Word("أخت صغرى","younger sister",R.drawable.family_younger_sister,R.raw.family_younger_sister));
+        words.add(new Word("جدة","grandmother",R.drawable.family_grandmother,R.raw.family_grandmother));
+        words.add(new Word("جد","grandfather",R.drawable.family_grandfather,R.raw.family_grandfather));
 
         WordAdapter wordAdapter = new WordAdapter(this,words,R.color.category_family,R.color.text_family);
         ListView listView = findViewById(R.id.list);
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -15,16 +15,16 @@ public class PhrasesActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
         //Create a list of words
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word ("إلى أين أنت ذاهب؟","Where are you going?"));
-        words.add(new Word( "ما هو اسمك؟","What is your name?"));
-        words.add(new Word("إسمي هو ...","My name is..."));
-        words.add(new Word("كيف تشعر؟","How are you feeling?"));
-        words.add(new Word("أشعر بشعور جيد.","I'm feeling good."));
-        words.add(new Word("هل أنت قادم؟","Are you coming?"));
-        words.add(new Word("نعم، أنا قادم.","Yes, I'm coming."));
-        words.add(new Word("أنا قادم.","i'm coming."));
-        words.add(new Word("لنذهب.","Let's go."));
-        words.add(new Word("تعال هنا.","Come here."));
+        words.add(new Word ("إلى أين أنت ذاهب؟","Where are you going?",R.raw.phrase_where_are_you_going));
+        words.add(new Word( "ما هو اسمك؟","What is your name?",R.raw.phrase_what_is_your_name));
+        words.add(new Word("إسمي هو ...","My name is...",R.raw.phrase_my_name_is));
+        words.add(new Word("كيف تشعر؟","How are you feeling?",R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("أشعر بشعور جيد.","I'm feeling good.",R.raw.phrase_im_feeling_good));
+        words.add(new Word("هل أنت قادم؟","Are you coming?",R.raw.phrase_are_you_coming));
+        words.add(new Word("نعم، أنا قادم.","Yes, I'm coming.",R.raw.phrase_yes_im_coming));
+        words.add(new Word("أنا قادم.","i'm coming.",R.raw.phrase_im_coming));
+        words.add(new Word("لنذهب.","Let's go.",R.raw.phrase_lets_go));
+        words.add(new Word("تعال هنا.","Come here.",R.raw.phrase_come_here));
 
         WordAdapter wordAdapter = new WordAdapter(this,words,R.color.category_phrases,R.color.text_phrases);
         ListView listView = findViewById(R.id.list);
